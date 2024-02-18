bca756d60b1d9825de6c40b5a09d914e5b3e4d1a
Yusuf Çalışkan <yusufocaliskan@gmail.com>
2024-02-18 22:06:35 +0300

    Escape and Input

---
+++ L2/escapes.py
@@ -0,0 +1,5 @@
+aa = "test"
+bb = "Deneme"
+print("-------- Test -------\n ")
+print("E-mail \t: test@gmail.com \nYazar\t: Test Yazar ")
+print("Blog\t: http://wwww.test.com")
---
+++ L2/input.py
@@ -0,0 +1,5 @@
+email = input("E-mail nedir? ")
+password = input("Sifre nedir? ")
+age = int(input("Kac yasindasin? "))
+
+print("E-Mail\t: " + email + "\nSifre \t: " + password, "\nYas\t:", str(age))
